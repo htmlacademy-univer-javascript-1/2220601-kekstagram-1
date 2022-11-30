c28dbbc27448f496fd059ee74d6ec08d76818a34
Роман <romanlabzin03@gmail.com>
2022-11-30 20:07:15 +0500

    m8-t2

--- js/data.js
+++ js/data.js
@@ -1,11 +1,13 @@
 import {getRandomNumber, getRandomArrayElement} from './util.js';
 
-const COMMENTS_COUNT = 5;
+const COMMENTS_COUNT = 25;
 const PHOTOS_COUNT = 25;
 const HASHTAGS_COUNT = 5;
 const COMMENTS_LENGTH = 140;
 const REG_EXP = /^#[A-Za-z0-9А-Яа-яЁё]{1,19}$/;
 const HASTAGS_LENGTH = 20;
+const UPLOAD_COMMENTS_COUNT = 5;
+const DEFAULT_COMMENTS_COUNT = 5;
 
 const NAMES = [
   'Семен',
@@ -33,12 +35,12 @@ const DESCRIPTIONS = [
   'Вауууу',
   'Тут могла быть ваша реклама...',
   'Мне лень придумывать описания',
-  'Ну типа...',
+  'Тупые козырьки',
   'Бравл старс',
   'Бешикташ',
   'Дота 2',
-  'Мидовый пудж - топ',
-  'УрФУ - норм уник'
+  'Момчик на пудже',
+  'урфу норм уник ))0'
 ];
 
 let idComment = 0;
@@ -55,10 +57,10 @@ const createPublication = () => ({
   url: `photos/${idPublication}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomNumber(15, 200),
-  comments: Array.from({length: COMMENTS_COUNT}, createComment)
+  comments: Array.from({length: getRandomNumber(1, COMMENTS_COUNT)}, createComment)
 });
 
 const createPhotos = () => Array.from({length: PHOTOS_COUNT}, createPublication);
 
 
-export {createPhotos, HASHTAGS_COUNT, COMMENTS_LENGTH, REG_EXP, HASTAGS_LENGTH};
+export {createPhotos, HASHTAGS_COUNT, COMMENTS_LENGTH, REG_EXP, HASTAGS_LENGTH, COMMENTS_COUNT, DEFAULT_COMMENTS_COUNT, UPLOAD_COMMENTS_COUNT};
