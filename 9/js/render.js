0b0efc197f363118aad3245656c477e016ecc385
Keks <keks@htmlacademy.ru>
2022-12-01 12:39:01 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/render.js
@@ -0,0 +1,28 @@
+import {createPhotos} from './data.js';
+import {renderBigPicture} from './render-big-picture.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragment = document.createDocumentFragment();
+const photos = createPhotos();
+
+const renderPicture = (image) => {
+  const picture = templateFragment.cloneNode(true);
+  picture.querySelector('.picture__img').src = image.url;
+  picture.querySelector('.picture__likes').textContent = image.likes;
+  picture.querySelector('.picture__comments').textContent = image.comments.length;
+  renderBigPicture(picture, image);
+
+  return picture;
+};
+
+const renderPictures = (pictures) => {
+  pictures.forEach((picture) => {
+    renderPicture(picture);
+    fragment.appendChild(renderPicture(picture));
+  });
+  picturesContainer.append(fragment);
+};
+
+renderPictures(photos);
